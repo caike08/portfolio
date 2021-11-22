ea87b0ed8a5e29c30c9e8c1f1c83eb012f0966ab
Carlos Motta <carlos.motta.br@socialbakers.com>
2021-11-21 21:11:24 -0300

    updated portfolio website

--- js/index.js
+++ js/index.js
@@ -1,24 +1,22 @@
 const init = () => {
-  document
-    .addEventListener('DOMContentLoaded', () => {
-      const date = document.getElementById('date');
-      date.innerHTML = new Date().getFullYear();
+  const date = document.querySelector('#date');
 
 
-      window.addEventListener('scroll', (e) => {
-        const distanceY = window.pageYOffset || document.documentElement.scrollTop;
-        const shrinkOn = 59;
-        const header = document.querySelector('header');
-        
-        if(distanceY > shrinkOn) {
-          header.classList.add('sticky');
-        }
-        else {
-          if(header.classList.contains('sticky')) {
-            header.classList.remove('sticky');
-          }
-        }
-    });
+  document.addEventListener('readystatechange', (event) => {
+    date.innerHTML = new Date().getFullYear();
+  });
+
+  document.addEventListener('scroll', (event) => {
+    const distanceY = window.pageYOffset || document.documentElement.scrollTop;
+    const shrinkOn = 240;
+    const header = document.querySelector('header');
+    
+    if(distanceY > shrinkOn) {
+      header.classList.add('sticky');
+    }
+    else {
+      header.classList.remove('sticky');
+    }
   })
 }
 
