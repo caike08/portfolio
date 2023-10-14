9357c7315a6bddfff84ae3918d5a7200e14528bb
Carlos Motta <carlos.motta.br@socialbakers.com>
2023-10-14 18:41:17 -0300

    use import.meta to import toasty.mp3

--- src/hooks/useToasty.ts
+++ src/hooks/useToasty.ts
@@ -1,11 +1,17 @@
 import { useEffect, useState } from 'react';
 
+// import.meta meta-property exposes context-specific metadata to a JavaScript module.
+// It contains information about the module, such as the module's URL.
+// toasty.mp3 is deployed to gh-pages, but isn't loaded directly.
+// based on: https://github.com/krau5/pomo/pull/9/commits/14866f670e2b5c63689beb75387c16157d636134
+const soundURL = new URL('/toasty.mp3', import.meta.url) // TODO: find a better way to import the mp3 file
+
 const useToasty = (animationDuration: number) => {
   const [audio, setAudio] = useState<HTMLAudioElement | null>(null)
   const [playing, setPlaying] = useState(false)
 
   useEffect(() => {
-    setAudio(new Audio('/toasty.mp3'))
+    setAudio(new Audio(soundURL.href))
   }, [])
 
   return {
